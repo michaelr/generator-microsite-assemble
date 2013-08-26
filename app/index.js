87c544785ff70c630048e322e4b15a9515fa9d6c
Michael Reddick <michael.reddick@gmail.com>
2013-08-26 17:05:38 -0500

    Skip perl deps if --skip-install

--- app/index.js
+++ app/index.js
@@ -3,13 +3,18 @@ var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var shell = require('shelljs');
+var chalk = require('chalk');
 
 
 var MicrositeAssembleGenerator = module.exports = function MicrositeAssembleGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
+  this.skipInstall = options['skip-install'];
+
   this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
+    this.installDependencies({ skipInstall: this.skipInstall });
+
+    console.log('Microsite generator finished. Run ' + chalk.yellow.bold('grunt dev') + ' to start dev server.');
   });
 
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
@@ -54,15 +59,21 @@ MicrositeAssembleGenerator.prototype.app = function app() {
 
 };
 
+MicrositeAssembleGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+  this.copy('gitignore', '.gitignore');
+};
+
 MicrositeAssembleGenerator.prototype.perldeps = function perldeps() {
+  if (this.skipInstall) {
+    console.log('\nRun ' + chalk.yellow.bold('carton install') + ' to install perl dependencies.');
+    return;
+  }
+
   var code = shell.exec('carton install').code;
   if (code) {
     throw new Error('carton install failed.');
   }
 };
 
-MicrositeAssembleGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
-  this.copy('gitignore', '.gitignore');
-};
