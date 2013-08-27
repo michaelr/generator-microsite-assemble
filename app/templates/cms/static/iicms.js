a679e448e4e9c8c96198e6f5a5bc535ba888c334
Michael Reddick <michael.reddick@gmail.com>
2013-08-27 13:09:14 -0500

    Fix jshint warnings

--- app/templates/cms/static/iicms.js
+++ app/templates/cms/static/iicms.js
@@ -1,75 +1,88 @@
-var app = angular.module("iicms", [] );
+'use strict';
 
-app.controller("iicmsCntrl", function($scope, $http, $q) {
-    $scope.activeEditor = null;
+var app = angular.module('iicms', []);
 
-    var lastPost = { then: function(f) { return f.apply(); } };
+app.controller('iicmsCntrl', function ($scope, $http) {
+  $scope.activeEditor = null;
 
-    $scope.showEditor = function(editor) {
-        if($scope.activeEditor){
-            $scope.activeEditor.hide();
+  var lastPost = {
+    then: function (f) {
+      return f.apply();
+    }
+  };
+
+  $scope.showEditor = function (editor) {
+    if ($scope.activeEditor) {
+      $scope.activeEditor.hide();
+    }
+    $scope.activeEditor = editor;
+    editor.show();
+    editor.find('textarea').focus();
+  };
+
+  $scope.saveFragment = function (fragment) {
+    lastPost = lastPost.then(function () {
+      return $http.post('/cms', JSON.stringify(fragment), {
+        headers: {
+          'Content-Type': 'application/json'
         }
-        $scope.activeEditor = editor;
-        editor.show();
-        editor.find('textarea').focus();
-    };
-
-    $scope.saveFragment = function(fragment) {
-        lastPost = lastPost.then(function() {
-            return $http.post('/cms', JSON.stringify(fragment), { headers: { 'Content-Type': 'application/json' }});
-        });
-    };
+      });
+    });
+  };
 });
 
-app.directive("iicmsfragment", ['$compile', function ($compile) {
+app.directive('iicmsfragment', ['$compile',
+  function ($compile) {
     return {
-        restrict: "E",
-        scope: true,
-        link: function(scope, elm, attrs) {
+      restrict: 'E',
+      scope: true,
+      link: function (scope, elm, attrs) {
 
-            var origContent = elm.html();
-            scope.content = origContent;
-            scope.fragmentPath = attrs.path;
+        var origContent = elm.html();
+        scope.content = origContent;
+        scope.fragmentPath = attrs.path;
 
-            var edit = angular.element("<div class='cms-edit'><span>edit</span></div>");
-            var editor = $compile(angular.element(" \
-              <div class='cms-editor'> \
-                <a href='#' class='close'>close</a> \
+        var edit = angular.element('<div class="cms-edit"><span>edit</span></div>');
+        var editor = $compile(angular.element(' \
+              <div class="cms-editor"> \
+                <a href="#" class="close">close</a> \
                 <h4>Fragment Editor</h4> \
                 <h5>{{fragmentPath}}</h5> \
-                <textarea type=text ng-model='content'></textarea> \
+                <textarea type=text ng-model="content"></textarea> \
               </div> \
-              "))(scope);
+              '))(scope);
 
-            angular.element('body').append(editor);
+        angular.element('body').append(editor);
 
-            elm.after(edit);
+        elm.after(edit);
 
-            editor.keyup(function(e){
-                if(e.keyCode === 27)
-                    editor.hide();
+        editor.keyup(function (e) {
+          if (e.keyCode === 27) {
+            editor.hide();
+          }
+        });
+
+        editor.find('.close').bind('click', function () {
+          editor.hide();
+        });
+
+        edit.find('span').bind('click', function () {
+          scope.showEditor(editor);
+        });
+
+        elm.after($compile(angular.element('<span ng-bind-html-unsafe="content" />'))(scope));
+        elm.remove();
+
+        scope.$watch('content', function (newVal, oldVal) {
+          if (newVal !== oldVal) {
+            scope.saveFragment({
+              fragment: attrs.path,
+              content: newVal
             });
+          }
+        });
 
-            editor.find('.close').bind("click", function () {
-                editor.hide();
-            });
-
-            edit.find('span').bind("click", function() {
-              scope.showEditor(editor);
-            });
-
-            elm.after($compile(angular.element("<span ng-bind-html-unsafe='content' />"))(scope));
-            elm.remove();
-
-            scope.$watch('content', function(newVal, oldVal) {
-              if( newVal !== oldVal ) {
-                scope.saveFragment({
-                    fragment: attrs.path,
-                    content: newVal
-                });
-              }
-            });
-
-        }
+      }
     };
-}]);
+  }
+]);
